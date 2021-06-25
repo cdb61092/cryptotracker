4d68a8c2f8832d4ecdf142e78623fea2ab28f0b9
Chase Buchholz <cdbuchholz10@gmail.com>
2021-06-25 17:24:38 -0500

    more work

--- src/Components/Rankings/Styles.js
+++ src/Components/Rankings/Styles.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+export { Th, Td } from "../../shared/Styles";
 
 export const Rankings = styled.div`
   width: 100%;
+  background-color: var(--light-bg);
 `;
 
 export const Heading = styled.h1`
@@ -9,4 +11,8 @@ export const Heading = styled.h1`
   color: white;
   font-size: 4rem;
   margin-bottom: 100px;
+`;
+
+export const SparklinesContainer = styled.div`
+  width: 250px;
 `;
